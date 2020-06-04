8c973967911418785587c6b55844779614e62e88
Nekrasova <anna.nekrasova@decathlon.com>
2020-06-04 13:41:57 +0300

    Flask_Project_Week_2

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
+### Import essential libraries
 from flask import Flask, render_template
-import templates
-import static.data
+import random
+
+### Import source data
+import static.data as data
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def test():
-    return render_template("index.html", title=static.data.my_dict["title"],
-                           line=static.data.my_dict["line"],
-                           heading=static.data.my_dict["heading"])
+def render_main():
+    return render_template('index.html',
+                           title=data.title,
+                           subtitle=data.subtitle,
+                           description=data.description,
+                           hotel_list=dict(random.sample(data.tours.items(), 6)),
+                           departures=data.departures
+                           )
 
+@app.route('/departures/<departure>/')
+def render_departure(departure):
+    return render_template('departure.html',
+                           title=data.title,
+                           departures=data.departures,
+                           hotel_list=dict(data.tours.items())
+                           )
+
+@app.route('/tours/<hotel_id>/')
+def render_tours(hotel_id):
+    return render_template('tour.html',
+                           title=data.title,
+                           departures=data.departures,
+                           hotel_name=data.tours[int(hotel_id)]['title'],
+                           hotel_country=data.tours[int(hotel_id)]['country'],
+                           hotel_stars=data.tours[int(hotel_id)]['stars'],
+                           hotel_departure=data.departures[data.tours[int(hotel_id)]['departure']],
+                           hotel_nights=data.tours[int(hotel_id)]['nights'],
+                           hotel_price=data.tours[int(hotel_id)]['price'],
+                           hotel_picture=data.tours[int(hotel_id)]['picture'],
+                           hotel_description=data.tours[int(hotel_id)]['description']
+                           )
+
+@app.route('/booking/')
+def render_booking():
+    return render_template('booking.html',
+                           title=data.title,
+                           departures=data.departures
+                           )
 
 app.run(debug=True)
